ef6fe89bf6b2489e60a2935ecaa6c039f7cbc342
Alyona <LoverTea31@gmail.com>
2023-10-11 15:15:30 +0700

    Добавьте файлы проекта.

---
+++ Sport/Pages/Auto.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sport.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Auto.xaml
+    /// </summary>
+    public partial class Auto : Page
+    {
+        public Auto()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuesrs_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Client());
+        }
+
+        private void btnEnter_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
